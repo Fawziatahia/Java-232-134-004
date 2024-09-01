a5f8810777ba0aa857f9006f1f4c7c8dc6b962a3
Fawziatahia <145826661+Fawziatahia@users.noreply.github.com>
2024-09-02 00:59:11 +0600

    git

---
+++ GradeBookTest.java
@@ -0,0 +1,15 @@
+package Chapter7;
+
+public class GradeBookTest {
+
+    public static void main(String[] args) {
+
+        int[] gradesArray = {87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+        GradeBook myGradeBook = new GradeBook("CS101 Introduction to Java Programming", gradesArray);
+
+        System.out.println("Welcome to the gradebook for "+myGradeBook.getCourseName());
+        myGradeBook.processGrades();
+
+    }
+
+}
