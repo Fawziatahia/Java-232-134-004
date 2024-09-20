cb39e118123f661efbced320a72a097830abab85
Fawziatahia <145826661+Fawziatahia@users.noreply.github.com>
2024-09-21 01:15:59 +0600

    git

---
+++ Dialog.java
@@ -0,0 +1,10 @@
+package Chapter3;
+
+import javax.swing.JOptionPane;
+public class Dialog {
+    
+     public static void main(String[] args)
+     {                    
+         JOptionPane.showMessageDialog(null, "Welcome to Java");
+     }
+}
---
+++ TestDraw.java
@@ -0,0 +1,15 @@
+package Chapter8;
+
+import javax.swing.JFrame;
+public class TestDraw {
+     public static void main(String[] args)
+     {
+         DrawPanel1 panel = new DrawPanel1();      
+         JFrame app = new JFrame();
+      
+         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         app.add(panel);
+         app.setSize(300, 300);
+         app.setVisible(true);
+     }
+}
